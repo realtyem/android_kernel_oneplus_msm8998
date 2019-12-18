3a0187d307c2136ca9d58f96dd22ee53216b10c1
Jason Little <realtyem@gmail.com>
2019-12-18 12:23:03 -0600

    Cpufreq: schedutil: restore defaults to down_rate_limit_us

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -820,7 +820,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 
         /* Hard-code some sane rate-limit values */
         tunables->up_rate_limit_us = 500;
-        tunables->down_rate_limit_us = 20000;
+        tunables->down_rate_limit_us = 1000;
 
 	tunables->iowait_boost_enable = false;
 
