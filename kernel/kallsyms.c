6431586adddf09ed8a8442b67964f07245939e65
Jason Little <realtyem@gmail.com>
2019-12-27 17:58:13 -0600

    kernel: kallsyms: switch strcpy to memcpy because I despise warnings. Even stupid ones.

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -366,8 +366,9 @@ static int __sprint_symbol(char *buffer, unsigned long address,
 	if (!name)
 		return sprintf(buffer, "0x%lx", address - symbol_offset);
 
-	if (name != buffer)
-		strcpy(buffer, name);
+	if (name != buffer) {
+		memcpy(buffer, name, sizeof(name)+1);
+	}
 	len = strlen(buffer);
 	offset -= symbol_offset;
 
