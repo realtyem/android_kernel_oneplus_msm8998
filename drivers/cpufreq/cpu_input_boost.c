a10d5b2adfceecb2e35478774906afab743f3ce4
Jason Little <realtyem@gmail.com>
2019-12-29 18:42:50 -0600

    cpu_input_boost: Add early wake boost on power button press
    Boost the CPU preemptively upon a power button press to improve wake-up
    speeds. The power button being pressed does not always translate to a
    wake, but the power cost from a such a brief boost should not be a
    deal-breaker.
    
    In testing, the device woke up 10 ms faster on average after this
    change.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    
    Switched out the existing solution for this one. Uses the new api

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -323,15 +323,11 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 {
 	struct boost_drv *b = handle->handler->private;
 
-	if (code == KEY_POWER && value == 1) {
-		if (get_boost_state(b) & SCREEN_OFF) {
-			pr_info("power-key boost\n");
-			__cpu_input_boost_kick_max(b, 1000);
-		}
-		return;
-	}
-
 	__cpu_input_boost_kick(b);
+
+	if (type == EV_KEY && code == KEY_POWER && value == 1 &&
+	    !(get_boost_state(b) & SCREEN_OFF))
+		__cpu_input_boost_kick_wake(b);
 
 	b->last_input_jiffies = jiffies;
 }
